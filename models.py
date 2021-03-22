f8b2f784c362dec13d23d420a46bf24f9a6bec7c
jtsuchiyama <jtsuchiyama@scu.edu>
2021-03-21 22:01:36 -1000

    Added timezone property to model class

--- models.py
+++ models.py
@@ -2,11 +2,12 @@ from flask_login import UserMixin
 from .db import Database
 
 class User(UserMixin): # inherits is_authenticated, is_actgive, is_anonymous, get_id()
-    def __init__(self,id,email,password,name):
+    def __init__(self,id,email,password,name, timezone):
         self.id = id
         self.email = email
         self.password = password
         self.name = name
+        self.timezone = timezone
 
     @staticmethod
     def get_user(param, type): # returns the user data based on if an email or id is passed
@@ -34,6 +35,6 @@ class User(UserMixin): # inherits is_authenticated, is_actgive, is_anonymous, ge
                         "SELECT * FROM public.user WHERE id='%s'" % (param)
                     )[0]
 
-        user = User(data[0],data[1],data[2],data[3])
+        user = User(data[0],data[1],data[2],data[3],data[4])
         return user
         
