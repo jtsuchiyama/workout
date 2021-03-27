b6d6afc81c9a8e6dba849fdadd7810a21f42d282
jtsuchiyama <jtsuchiyama@scu.edu>
2021-03-26 16:31:29 -1000

    test

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_migrate import Migrate
 from flask_login import LoginManager
 from os import environ, path
 from dotenv import load_dotenv
@@ -13,7 +14,7 @@ login_manager = LoginManager()
 login_manager.login_view = "auth.login"
 login_manager.init_app(app)
 
-from .models import User
+from models import User
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -21,9 +22,9 @@ def load_user(user_id):
     return User.get_user(int(user_id),"id")
 
 # Blueprint for auth routes in our app
-from .auth import auth as auth_blueprint
+from auth import auth as auth_blueprint
 app.register_blueprint(auth_blueprint)
 
 # Blueprint for non-auth parts of app
-from .main import main as main_blueprint
+from main import main as main_blueprint
 app.register_blueprint(main_blueprint)
